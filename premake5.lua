5711c2949c61cdc006236e48c7e329335f9f9154
Rewriter00x <84532061+Rewriter00x@users.noreply.github.com>
2024-10-25 23:59:44 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir   ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir      ("int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+
+    filter { "system:windows", "configurations:Debug" }
+        runtime "Debug"
+
+    filter { "system:windows", "configurations:Release" }
+        runtime "Release"
+
+    filter { "system:windows", "configurations:Dist" }
+        runtime "Release"
